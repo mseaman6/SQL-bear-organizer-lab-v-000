76b85b8bb928582fd4a7fe5757207d522b93843f
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-28 01:21:33 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("BooBoo", 4, 0, "brown", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Smokey", 52, 0, "brown", "helpful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fozzie", 36, 0, "tan", "goofy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Paddington", 7, 0, "light-brown", "nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Winnie", 45, 0, "tan", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Baloo", 60, 0, "black", "fatherly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Yogi", 37, 0, "brown", "mischievous", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, 1, "chestnut", "aggresive", 1);
